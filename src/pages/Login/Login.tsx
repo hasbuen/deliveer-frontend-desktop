2e4bd5f7dc01709f33bca599946c68bf932e3769
hasbuen <juliocesar.ovidiobueno@outlook.com>
2024-10-24 14:04:09 -0300

    UP

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -44,7 +44,7 @@ function Login() {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-screen flex-1 flex-col items-center justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           alt="DeliVeer"
